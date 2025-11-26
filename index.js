f465e0ac3f87321edcf254985c83af2ba4fa460c
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-26 10:44:48 +0700

    menambahkan index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+//const apiRoutes = require('./routes/api');
+const apiRoutes = require('./routes/api');
+
+connectDatabase
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//app.use('/api/v1', apiRoutes);
+app.use('/api/v1', apiRoute);
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
